de879c5ac4ad9c5110091dcb751b7eba305ea2e7
Moez <54633181+moez-IT-DEV@users.noreply.github.com>
2026-09-06 13:07:03 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:test1/src/database/firebase_options.dart';
 
 import 'package:test1/src/Utils/routes.dart';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
